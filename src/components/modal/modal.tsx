102eefa2c600d925ad5fdd6384ee2cf90b924b19
Bozhidar Tonev <bojidar.tonev.9@gmail.com>
2024-09-07 01:28:11 +0300

    implemented modal fade it and out

--- src/components/modal/modal.tsx
+++ src/components/modal/modal.tsx
@@ -1,12 +1,12 @@
-import {ReactNode, useCallback} from 'react';
-import {closeModal, ModalTypes} from "../../redux/features/modal-slice.ts";
+import { ReactNode, useCallback } from 'react';
+import { closeModal, ModalTypes } from "../../redux/features/modal-slice.ts";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
 import AuthModal from "./modal-types/auth-modal.tsx";
 import AddDirectoryModal from "./modal-types/add-directory-modal.tsx";
 import AddFileModal from "./modal-types/add-file-modal.tsx";
 import DeleteFileModal from "./modal-types/delete-file-modal.tsx";
-import {useAppDispatch, useAppSelector} from "../../redux/store.ts";
+import { useAppDispatch, useAppSelector } from "../../redux/store.ts";
 import ViewFullContentModal from "./modal-types/view-full-content-modal.tsx";
 import './modal.css';
 
@@ -17,28 +17,28 @@ const Modal = (): ReactNode | null => {
     const modalContent = useCallback(() => {
         switch (modalType) {
             case ModalTypes.AuthModal:
-                return <AuthModal />
+                return <AuthModal />;
             case ModalTypes.AddDirectoryModal:
                 return <AddDirectoryModal />;
             case ModalTypes.AddFileModal:
                 return <AddFileModal />;
             case ModalTypes.DeleteFileModal:
-                return <DeleteFileModal />
+                return <DeleteFileModal />;
             case ModalTypes.ViewFileFullContentModal:
-                return <ViewFullContentModal />
+                return <ViewFullContentModal />;
             default:
-                return <div/>;
+                return <div />;
         }
-    }, [modalType])
-
-    if (!isOpen) {
-        return null;
-    }
+    }, [modalType]);
 
     return (
         <div className={`modal-overlay ${isOpen ? 'fade-in' : 'fade-out'}`}>
             <div className="modal-content">
-                <FontAwesomeIcon icon={faXmark} onClick={() => dispatch(closeModal())} className="close-modal-icon" />
+                <FontAwesomeIcon
+                    icon={faXmark}
+                    onClick={() => dispatch(closeModal())}
+                    className="close-modal-icon"
+                />
                 <div className="modal-title">
                     {title}
                     {subTitle && <div>{subTitle}</div>}
@@ -47,6 +47,6 @@ const Modal = (): ReactNode | null => {
             </div>
         </div>
     );
-}
+};
 
 export default Modal;
