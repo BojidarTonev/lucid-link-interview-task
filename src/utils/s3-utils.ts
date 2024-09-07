ac149d02c6c6610eb2920ffaba386060f87d0f49
Bozhidar Tonev <bojidar.tonev.9@gmail.com>
2024-09-07 17:06:36 +0300

    additional check implemented

--- src/utils/s3-utils.ts
+++ src/utils/s3-utils.ts
@@ -6,6 +6,7 @@ import { IS3Credentials } from "../redux/features/s3-client-slice.ts";
 // would not be present on repo clone, default value is passed in
 // order to have the app running with additional layer of security
 // and no need to manually create .env file and pass data to it on clone
+// but that is not how its done, the variable value would only come from .env file
 const ENCRYPTION_KEY = import.meta.env.VITE_ENCRYPTION_KEY || 'some_key';
 
 export const getS3ClientAndConfig = (getState: () => any) => {
@@ -32,6 +33,10 @@ export const getS3ClientAndConfig = (getState: () => any) => {
 
     if (!bucketName) {
         throw new Error('Bucket name is not specified.');
+    }
+
+    if (!s3Client) {
+        throw new Error('S3 Client not instantiated!');
     }
 
     return { s3Client, config, bucketName: config.bucketName };
